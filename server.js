908aff1e0e4211a744b2719e682ac7553bf0e0fd
Helio Leao <hmtleao@hotmail.com>
2023-01-28 18:41:20 -0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-// if(process.env.NODE_ENV !== 'production') {
-//     require('dotenv').parse()
-// }
+if(process.env.NODE_ENV !== 'production') {
+    // require('dotenv').parse()
+    require('dotenv').config()
+}
 
+
+// IMPORTS
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+// END IMPORTS
 
+// ROUTERS
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+// END ROUTERS
 
+// CONFIG
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+// END CONFIG
 
+// MONGOOSE
 const mongoose = require('mongoose')
+const { urlencoded } = require('body-parser')
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost/mybrary', {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
+// END MONGOOSE
 
+// ROUTES
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+// END ROUTES
 
 
-// app.listen(process.env.PORT || 3000)
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
